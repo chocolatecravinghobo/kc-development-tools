be4acc0308cb501b3461879686ded22c4703d0b4
SkywalkerJi <skywalkerji.cn@gmail.com>
2021-09-22 00:31:37 +0800

    add iteams

--- items.js
+++ items.js
@@ -1,126 +1,145 @@
-var itemtypenames
-if (itemtypenames == undefined) itemtypenames = [ "Small Caliber Main Gun",
-"Medium Caliber Main Gun",
-"Large Caliber Main Gun",
-"Secondary Gun",
-"Anti-Aircraft Gun",
-"Anti-Aircraft Shell",
-"Armor Piercing Shell",
-"Torpedos & Submarine",
-"Carrier-Based Fighter",
-"Carrier-Based Torpedo Bomber",
-"Carrier-Based Dive Bomber",
-"Carrier-Based Reconn",
-"Seaplane",
-"Radar",
-"Anti-submarine Warfare",
-"Engine",
-"Extra Armor",
-"Land-Based Attack Aircraft",
-"Supply Transport Container" ]
+var itemtypenames;
+if (itemtypenames == undefined)
+  itemtypenames = [
+    "Small Caliber Main Gun",
+    "Medium Caliber Main Gun",
+    "Large Caliber Main Gun",
+    "Secondary Gun",
+    "Anti-Aircraft Gun", 
+    "Anti-Aircraft Shell",
+    "Armor Piercing Shell",
+    "Torpedos & Submarine",
+    "Carrier-Based Fighter",
+    "Carrier-Based Torpedo Bomber",
+    "Carrier-Based Dive Bomber",
+    "Carrier-Based Reconn",
+    "Seaplane",
+    "Radar",
+    "Anti-submarine Warfare",
+    "Engine",
+    "Extra Armor",
+    "Land-Based Attack Aircraft",
+    "Supply Transport Container",
+	"Anti-Air Fire Director",
+	"Land-based Fighter",
+  ];
 
 // Id, Name, Rarity, Internal Rarity, Type, Break Materials[, is AA]
 var items = [
-	[1, "12cm単装砲", 1, 0, 0, [0, 1, 1, 0]],
-	[2, "12.7cm連装砲", 1, 0, 0, [0, 1, 2, 0]],
-	[3, "10cm連装高角砲", 2, 1, 0, [0, 1, 3, 0], true],
-	[4, "14cm単装砲", 1, 0, 1, [0, 2, 1, 0]],
-	[5, "15.5cm三連装砲", 2, 0, 1, [0, 2, 5, 0]],
-	[6, "20.3cm連装砲", 2, 0, 1, [0, 3, 4, 0]],
-	[65, "15.2cm連装砲", 2, 2, 1, [0, 2, 3, 0]],
-	[7, "35.6cm連装砲", 1, 0, 2, [0, 10, 15, 0]],
-	[8, "41cm連装砲", 2, 1, 2, [0, 12, 20, 0]],
-	[9, "46cm三連装砲", 3, 2, 2, [0, 24, 25, 0]],
-	[10, "12.7cm連装高角砲", 1, 0, 3, [0, 2, 2, 0], true],
-	[11, "15.2cm単装砲", 1, 0, 3, [0, 2, 2, 0]],
-	[12, "15.5cm三連装副砲", 2, 1, 3, [0, 2, 5, 0]],
-	[66, "8cm高角砲", 3, 3, 3, [0, 1, 2, 0], true],
-	[37, "7.7mm機銃", 1, 0, 4, [0, 1, 1, 0]],
-	[38, "12.7mm単装機銃", 1, 0, 4, [0, 1, 1, 0]],
-	[39, "25mm連装機銃", 2, 0, 4, [0, 2, 1, 0]],
-	[40, "25mm三連装機銃", 2, 1, 4, [0, 3, 1, 0]],
-	[49, "25mm単装機銃", 2, 1, 4, [0, 1, 1, 0]],
-	[35, "三式弾", 2, 0, 5, [0, 9, 6, 3]],
-	[36, "九一式徹甲弾", 3, 1, 6, [0, 3, 9, 0]],
-	[13, "61cm三連装魚雷", 1, 0, 7, [1, 1, 1, 0]],
-	[14, "61cm四連装魚雷", 1, 0, 7, [1, 2, 2, 0]],
-	[15, "61cm四連装(酸素)魚雷", 2, 1, 7, [2, 2, 2, 0]],
-	[41, "甲標的", 1, 1, 7, [0, 7, 7, 0]],
-	[19, "九六式艦戦", 1, 0, 8, [1, 1, 0, 1]],
-	[20, "零式艦戦21型", 1, 0, 8, [1, 1, 0, 2]],
-	[21, "零式艦戦52型", 2, 1, 8, [1, 2, 0, 3]],
-	[22, "試製烈風 後期型", 3, 3, 8, [2, 2, 0, 9]],
-	[55, "紫電改二", 3, 2, 8, [2, 2, 0, 7]],
-	[181, "零式艦戦32型", 2, 3, 8, [1, 2, 0, 2]],
-	[16, "九七式艦攻", 1, 0, 9, [1, 1, 0, 2]],
-	[17, "天山", 2, 1, 9, [2, 4, 0, 4]],
-	[18, "流星", 3, 2, 9, [2, 5, 0, 10]],
-	[52, "流星改", 3, 3, 9, [2, 6, 0, 10]],
-	[23, "九九式艦爆", 1, 0, 10, [1, 1, 0, 2]],
-	[24, "彗星", 2, 1, 10, [2, 3, 0, 3]],
-	[57, "彗星一二型甲", 3, 2, 10, [2, 3, 0, 4]],
-	[60, "零式艦戦62型(爆戦)", 3, 2, 10, [1, 3, 0, 3]],
-	[54, "彩雲", 3, 2, 11, [2, 0, 0, 11]],
-	[61, "二式艦上偵察機", 2, 1, 11, [3, 1, 0, 13]],
-	[25, "零式水上偵察機", 1, 1, 12, [1, 1, 0, 2]],
-	[26, "瑞雲", 2, 1, 12, [2, 3, 0, 5]],
-	[59, "零式水上観測機", 2, 1, 12, [1, 1, 0, 2]],
-	[163, "Ro.43水偵", 3, 3, 12, [1, 1, 0, 2]],
-	[27, "13号対空電探", 2, 1, 13, [0, 0, 10, 10]],
-	[28, "22号対水上電探", 2, 1, 13, [0, 0, 15, 15]],
-	[29, "33号対水上電探", 3, 2, 13, [0, 0, 20, 15]],
-	[30, "21号対空電探", 2, 2, 13, [0, 0, 20, 20]],
-	[31, "32号対水上電探", 3, 3, 13, [0, 0, 20, 25]],
-	[32, "42号対空電探", 3, 4, 13, [0, 0, 25, 25]],
-	[44, "九四式爆雷投射機", 1, 0, 14, [0, 2, 1, 1]],
-	[45, "三式爆雷投射機", 3, 2, 14, [0, 3, 1, 1]],
-	[46, "九三式水中聴音機", 1, 0, 14, [0, 0, 1, 1]],
-	[47, "三式水中探信儀", 3, 2, 14, [0, 0, 1, 2]],
-	[33, "改良型艦本式タービン", 1, 0, 15, [10, 0, 10, 0]],
-	[34, "強化型艦本式缶", 2, 1, 15, [10, 0, 20, 0]],
-	[72, "増設バルジ(中型艦)", 2, 2, 16, [0, 0, 12, 0]],
-	[73, "増設バルジ(大型艦)", 2, 2, 16, [0, 0, 30, 0]],
-	[168, "九六式陸攻", 1, 1, 17, [7, 4, 0, 12]],
-	[75, "ドラム缶(輸送用)", 1, 0, 18, [0, 0, 1, 0]]
-]
+  [1, "12cm単装砲", 1, 0, 0, [0, 1, 1, 0]],
+  [2, "12.7cm連装砲", 1, 0, 0, [0, 1, 2, 0]],
+  [3, "10cm連装高角砲", 2, 1, 0, [0, 1, 3, 0], true],
+  [4, "14cm単装砲", 1, 0, 1, [0, 2, 1, 0]],
+  [5, "15.5cm三連装砲", 2, 0, 1, [0, 2, 5, 0]],
+  [6, "20.3cm連装砲", 2, 0, 1, [0, 3, 4, 0]],
+  [7, "35.6cm連装砲", 1, 0, 2, [0, 10, 15, 0]],
+  [8, "41cm連装砲", 2, 1, 2, [0, 12, 20, 0]],
+  [9, "46cm三連装砲", 3, 2, 2, [0, 24, 25, 0]],
+  [10, "12.7cm連装高角砲", 1, 0, 3, [0, 2, 2, 0], true],
+  [11, "15.2cm単装砲", 1, 0, 3, [0, 2, 2, 0]],
+  [12, "15.5cm三連装副砲", 2, 1, 3, [0, 2, 5, 0]],
+  [13, "61cm三連装魚雷", 1, 0, 7, [1, 1, 1, 0]],
+  [14, "61cm四連装魚雷", 1, 0, 7, [1, 2, 2, 0]],
+  [15, "61cm四連装(酸素)魚雷", 2, 1, 7, [2, 2, 2, 0]],
+  [16, "九七式艦攻", 1, 0, 9, [1, 1, 0, 2]],
+  [17, "天山", 2, 1, 9, [2, 4, 0, 4]],
+  [18, "流星", 3, 2, 9, [2, 5, 0, 10]],
+  [19, "九六式艦戦", 1, 0, 8, [1, 1, 0, 1]],
+  [20, "零式艦戦21型", 1, 0, 8, [1, 1, 0, 2]],
+  [21, "零式艦戦52型", 2, 1, 8, [1, 2, 0, 3]],
+  [22, "試製烈風 後期型", 3, 3, 8, [2, 2, 0, 9]],
+  [23, "九九式艦爆", 1, 0, 10, [1, 1, 0, 2]],
+  [24, "彗星", 2, 1, 10, [2, 3, 0, 3]],
+  [25, "零式水上偵察機", 1, 1, 12, [1, 1, 0, 2]],
+  [26, "瑞雲", 2, 1, 12, [2, 3, 0, 5]],
+  [27, "13号対空電探", 2, 1, 13, [0, 0, 10, 10]],
+  [28, "22号対水上電探", 2, 1, 13, [0, 0, 15, 15]],
+  [29, "33号対水上電探", 3, 2, 13, [0, 0, 20, 15]],
+  [30, "21号対空電探", 2, 2, 13, [0, 0, 20, 20]],
+  [31, "32号対水上電探", 3, 3, 13, [0, 0, 20, 25]],
+  [32, "42号対空電探", 3, 4, 13, [0, 0, 25, 25]],
+  [33, "改良型艦本式タービン", 1, 0, 15, [10, 0, 10, 0]],
+  [34, "強化型艦本式缶", 2, 1, 15, [10, 0, 20, 0]],
+  [35, "三式弾", 2, 0, 5, [0, 9, 6, 3]],
+  [36, "九一式徹甲弾", 3, 1, 6, [0, 3, 9, 0]],
+  [37, "7.7mm機銃", 1, 0, 4, [0, 1, 1, 0]],
+  [38, "12.7mm単装機銃", 1, 0, 4, [0, 1, 1, 0]],
+  [39, "25mm連装機銃", 2, 0, 4, [0, 2, 1, 0]],
+  [40, "25mm三連装機銃", 2, 1, 4, [0, 3, 1, 0]],
+  [41, "甲標的", 1, 1, 7, [0, 7, 7, 0]],
+  [44, "九四式爆雷投射機", 1, 0, 14, [0, 2, 1, 1]],
+  [45, "三式爆雷投射機", 3, 2, 14, [0, 3, 1, 1]],
+  [46, "九三式水中聴音機", 1, 0, 14, [0, 0, 1, 1]],
+  [47, "三式水中探信儀", 3, 2, 14, [0, 0, 1, 2]],
+  [49, "25mm単装機銃", 2, 1, 4, [0, 1, 1, 0]],
+  [51, "12cm30連装噴進砲", 4, 2, 4, [0, 4, 2, 2]],
+  [52, "流星改", 3, 3, 9, [2, 6, 0, 10]],
+  [54, "彩雲", 3, 2, 11, [2, 0, 0, 11]],
+  [55, "紫電改二", 3, 2, 8, [2, 2, 0, 7]],
+  [57, "彗星一二型甲", 3, 2, 10, [2, 3, 0, 4]],
+  [59, "零式水上観測機", 2, 1, 12, [1, 1, 0, 2]],
+  [60, "零式艦戦62型(爆戦)", 3, 2, 10, [1, 3, 0, 3]],
+  [61, "二式艦上偵察機", 2, 1, 11, [3, 1, 0, 13]],
+  [65, "15.2cm連装砲", 2, 2, 1, [0, 2, 3, 0]],
+  [66, "8cm高角砲", 3, 3, 3, [0, 1, 2, 0], true],
+  [72, "増設バルジ(中型艦)", 2, 2, 16, [0, 0, 12, 0]],
+  [73, "増設バルジ(大型艦)", 2, 2, 16, [0, 0, 30, 0]],
+  [75, "ドラム缶(輸送用)", 1, 0, 18, [0, 0, 1, 0]],
+  [90, "20.3cm(2号)連装砲", 2, 1, 1, [0, 3, 4, 0]],
+  [120, "91式高射装置", 1, 1, 19, [0, 1, 0, 1]],
+  [181, "零式艦戦32型", 2, 3, 8, [1, 2, 0, 2]],
+  [226, "九五式爆雷", 1, 1, 14, [1, 2, 0, 1]],
+  [78, "12.7cm単装砲", 1, 0, 0, [1, 1, 1, 1]],
+  [147, "120mm/50 連装砲", 2, 2, 0, [0, 1, 2, 0]],
+  [163, "Ro.43水偵", 3, 3, 12, [1, 1, 0, 2]],
+  [168, "九六式陸攻", 1, 1, 17, [7, 4, 0, 12]],
+  [194, "Laté 298B", 3, 3, 12, [2, 4, 0, 5]],
+  [195, "SBD", 2, 2, 10, [1, 3, 0, 2]],
+  [197, "F4F-3", 2, 2, 8, [1, 2, 0, 2]],
+  [207, "瑞雲(六三一空)", 3, 3, 12, [2, 3, 0, 5]],
+  [242, "Swordfish", 2, 2, 9, [1, 1, 0, 1]],
+  [249, "Fulmar", 2, 2, 8, [1, 2, 0, 2]],
+  [250, "Spitfire Mk.I", 3, 3, 20, [1, 2, 0, 4]],
+];
 
-var materialNames = ["Fuel", "Ammunition", "Steel", "Bauxite"]
+var materialNames = ["Fuel", "Ammunition", "Steel", "Bauxite"];
 
 function developResult(id, percentage, materials, hqlevel) {
-	this.id = id
-	this.percentage = percentage
-	this.successful = true
-	for (var i = 0; i < 4; ++i) {
-		if (items[id][5][i] * 10 > materials[i]) {
-			this.successful = false
-			break
-		}
-	}
-	if (items[id][3] * 10 > hqlevel) this.successful = false
+  this.id = id;
+  this.percentage = percentage;
+  this.successful = true;
+  for (var i = 0; i < 4; ++i) {
+    if (items[id][5][i] * 10 > materials[i]) {
+      this.successful = false;
+      break;
+    }
+  }
+  if (items[id][3] * 10 > hqlevel) this.successful = false;
 
-	this.toTRNode = function() {
-		var node = document.createElement("tr")
-		if (this.successful) node.className = "success"
-		else {
-			node.className = "fail"
-			var title = "Require"
-			for (var i = 0; i < 4; ++i) {
-				if (items[id][5][i] * 10 > materials[i]) {
-					title += "\n" + getString(materialNames[i]) + " ≥ " + (items[id][5][i] * 10)
-				}
-			}
-			if (items[id][3] * 10 > hqlevel) {
-				title += "\n" + getString("HQ Level") + " ≥ " + (items[id][3] * 10)
-			}
-			node.title = getString(title)
-		}
-		var tdnode = document.createElement("td")
-		tdnode.className = "type" + (items[id][6] ? 4 : items[id][4])
-		tdnode.appendChild(document.createTextNode(items[id][1]))
-		node.appendChild(tdnode)
-		tdnode = document.createElement("td")
-		tdnode.appendChild(document.createTextNode(percentage + "%"))
-		node.appendChild(tdnode)
-		return node
-	}
+  this.toTRNode = function () {
+    var node = document.createElement("tr");
+    if (this.successful) node.className = "success";
+    else {
+      node.className = "fail";
+      var title = "Require";
+      for (var i = 0; i < 4; ++i) {
+        if (items[id][5][i] * 10 > materials[i]) {
+          title +=
+            "\n" + getString(materialNames[i]) + " ≥ " + items[id][5][i] * 10;
+        }
+      }
+      if (items[id][3] * 10 > hqlevel) {
+        title += "\n" + getString("HQ Level") + " ≥ " + items[id][3] * 10;
+      }
+      node.title = getString(title);
+    }
+    var tdnode = document.createElement("td");
+    tdnode.className = "type" + (items[id][6] ? 4 : items[id][4]);
+    tdnode.appendChild(document.createTextNode(items[id][1]));
+    node.appendChild(tdnode);
+    tdnode = document.createElement("td");
+    tdnode.appendChild(document.createTextNode(percentage + "%"));
+    node.appendChild(tdnode);
+    return node;
+  };
 }
--- lang.js
+++ lang.js
@@ -1,228 +1,267 @@
-(function(){
-	var lang = window.location.search.substr(1)
-	if (["en","zh","zh-tw","ja"].indexOf(lang) < 0) {
-		lang = (navigator.browserLanguage ? navigator.browserLanguage : navigator.language).toLowerCase()
-		if (lang.indexOf("zh-tw") >= 0) lang = "zh-tw"
-		else if (lang.indexOf("zh") >= 0) lang = "zh"
-		else if (lang.indexOf("ja") >= 0) lang = "ja"
-		else if (navigator.languages != undefined) { 
-			for (var i = 0; i < navigator.languages.length; ++i) {
-				if (navigator.languages[i].toLowerCase().indexOf("zh-tw") >= 0) {
-					lang = "zh-tw"
-					break
-				}
-				if (navigator.languages[i].indexOf("zh") >= 0) {
-					lang = "zh"
-					break
-				}
-				if (navigator.languages[i].indexOf("ja") >= 0) {
-					lang = "ja"
-					break
-				}
-				if (navigator.languages[i].indexOf("en") >= 0) break
-			}
-			if ((lang != "zh") && (lang != "ja")) lang = "en"
-		}
-	}
-	if (lang == "zh") {
-document.title = "舰队Collection 开发工具"
-$(function(){document.body.lang = "zh"})
-dict = { "KanColle Development Tools": "舰队Collection 开发工具",
-"Development Simulator & Recipe Generator": "开发模拟器 & 复合公式生成器",
-"Development Simulator": "开发模拟器",
-"Recipe Generator": "复合公式生成器",
-"Probability Table": "概率一览表",
-"Fuel": "油",
-"Ammunition": "弹",
-"Ammu": "弹",
-"Steel": "钢",
-"Bauxite": "铝",
-"Dev. Mat.": "开发资材",
-"Secretary": "秘书舰",
-"HQ Level": "提督等级",
-"Torpedo Family": "水雷系",
-"Gunboat Family": "炮舰系",
-"Carrier Family": "航母系",
-"Require": "需要",
-"Min ": "最低",
-"No Result": "无结果",
-"Details": "详细",
-"Expected Resource Consumption": "期望出货消耗",
-"Get Target": "出货",
-"Develop": "开发",
-"Success Rate": "成功率",
-"Failure Rate": "失败率",
-"Name": "名称",
-"Percentage": "概率",
-"Failed": "失败",
-"Internal": "内部",
-"Rarity": "稀有度",
-"(\\d{1,2}%) when (.*?) is available": "$2 加入列表时为 $1",
-"Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)": "仅资源投入达到\n$1\n时加入列表",
-" ?Italian Flagship": "意舰旗舰",
-"No records for Luigi Torelli and Aquila": "Luigi Torelli、Aquila 无出货记录",
-"Project Homepage": "项目页面",
-"Feedback": "反馈",
-"Data Source\\(Chinese\\)": "数据来源" }
+(function () {
+  var lang = window.location.search.substr(1);
+  if (["en", "zh", "zh-tw", "ja"].indexOf(lang) < 0) {
+    lang = (
+      navigator.browserLanguage ? navigator.browserLanguage : navigator.language
+    ).toLowerCase();
+    if (lang.indexOf("zh-tw") >= 0) lang = "zh-tw";
+    else if (lang.indexOf("zh") >= 0) lang = "zh";
+    else if (lang.indexOf("ja") >= 0) lang = "ja";
+    else if (navigator.languages != undefined) {
+      for (var i = 0; i < navigator.languages.length; ++i) {
+        if (navigator.languages[i].toLowerCase().indexOf("zh-tw") >= 0) {
+          lang = "zh-tw";
+          break;
+        }
+        if (navigator.languages[i].indexOf("zh") >= 0) {
+          lang = "zh";
+          break;
+        }
+        if (navigator.languages[i].indexOf("ja") >= 0) {
+          lang = "ja";
+          break;
+        }
+        if (navigator.languages[i].indexOf("en") >= 0) break;
+      }
+      if (lang != "zh" && lang != "ja") lang = "en";
+    }
+  }
+  if (lang == "zh") {
+    document.title = "舰队Collection 开发工具";
+    $(function () {
+      document.body.lang = "zh";
+    });
+    dict = {
+      "KanColle Development Tools": "舰队Collection 开发工具",
+      "Development Simulator & Recipe Generator": "开发模拟器 & 复合公式生成器",
+      "Development Simulator": "开发模拟器",
+      "Recipe Generator": "复合公式生成器",
+      "Probability Table": "概率一览表",
+      Fuel: "油",
+      Ammunition: "弹",
+      Ammu: "弹",
+      Steel: "钢",
+      Bauxite: "铝",
+      "Dev. Mat.": "开发资材",
+      Secretary: "秘书舰",
+      "HQ Level": "提督等级",
+      "Torpedo Family": "水雷系",
+      "Gunboat Family": "炮舰系",
+      "Carrier Family": "航母系",
+      Require: "需要",
+      "Min ": "最低",
+      "No Result": "无结果",
+      Details: "详细",
+      "Expected Resource Consumption": "期望出货消耗",
+      "Get Target": "出货",
+      Develop: "开发",
+      "Success Rate": "成功率",
+      "Failure Rate": "失败率",
+      Name: "名称",
+      Percentage: "概率",
+      Failed: "失败",
+      Internal: "内部",
+      Rarity: "稀有度",
+      "(\\d{1,2}%) when (.*?) is available": "$2 加入列表时为 $1",
+      "Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)":
+        "仅资源投入达到\n$1\n时加入列表",
+      " ?Italian Flagship": "意舰旗舰",
+      "No records for Luigi Torelli and Aquila":
+        "Luigi Torelli、Aquila 无出货记录",
+      "Project Homepage": "项目页面",
+      Feedback: "反馈",
+      "Data Source\\(Chinese\\)": "数据来源",
+    };
 
-itemtypenames = [ "小口径主炮",
-"中口径主炮",
-"大口径主炮",
-"副炮",
-"机枪",
-"对空强化弹",
-"穿甲弹",
-"鱼雷 & 微型潜艇",
-"舰载战斗机",
-"舰载鱼雷机",
-"舰载轰炸机",
-"舰载侦查机",
-"水上机",
-"电探",
-"对潜装备",
-"引擎",
-"增设装甲板",
-"陆基攻击机",
-"简易运输桶" ]
-	} else if (lang == "zh-tw") {
-document.title = "艦隊Collection 開發工具"
-$(function(){document.body.lang = "zh-tw"})
-dict = { "KanColle Development Tools": "艦隊Collection 開發工具",
-"Development Simulator & Recipe Generator": "開發模擬器 & 複合公式生成器",
-"Development Simulator": "開發模擬器",
-"Recipe Generator": "複合公式生成器",
-"Probability Table": "概率一覽表",
-"Fuel": "燃",
-"Ammunition": "彈",
-"Ammu": "彈",
-"Steel": "鋼",
-"Bauxite": "鋁",
-"Dev. Mat.": "開發資材",
-"Secretary": "秘書艦",
-"HQ Level": "提督等級",
-"Torpedo Family": "水雷系",
-"Gunboat Family": "炮艦系",
-"Carrier Family": "航母系",
-"Require": "需要",
-"Min ": "最低",
-"No Result": "無結果",
-"Details": "詳細",
-"Expected Resource Consumption": "期望出貨消耗",
-"Get Target": "出貨",
-"Develop": "開發",
-"Success Rate": "成功率",
-"Failure Rate": "失敗率",
-"Name": "名稱",
-"Percentage": "概率",
-"Failed": "失敗",
-"Internal": "内部",
-"Rarity": "稀有度",
-"(\\d{1,2}%) when (.*?) is available": "$2 加入列表時為 $1",
-"Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)": "僅資源投入達到\n$1\n時加入列表",
-" ?Italian Flagship": "意艦旗艦",
-"No records for Luigi Torelli and Aquila": "Luigi Torelli、Aquila 無出貨記錄",
-"Project Homepage": "項目頁面",
-"Feedback": "反饋",
-"Data Source\\(Chinese\\)": "數據來源" }
+    itemtypenames = [
+      "小口径主炮",
+      "中口径主炮",
+      "大口径主炮",
+      "副炮",
+      "机枪",
+      "对空强化弹",
+      "穿甲弹",
+      "鱼雷 & 微型潜艇",
+      "舰载战斗机",
+      "舰载鱼雷机",
+      "舰载轰炸机",
+      "舰载侦查机",
+      "水上机",
+      "电探",
+      "对潜装备",
+      "引擎",
+      "增设装甲板",
+      "陆基攻击机",
+      "简易运输桶",
+	  "高射装置",
+	  "陆军战斗机",
+    ];
+  } else if (lang == "zh-tw") {
+    document.title = "艦隊Collection 開發工具";
+    $(function () {
+      document.body.lang = "zh-tw";
+    });
+    dict = {
+      "KanColle Development Tools": "艦隊Collection 開發工具",
+      "Development Simulator & Recipe Generator": "開發模擬器 & 複合公式生成器",
+      "Development Simulator": "開發模擬器",
+      "Recipe Generator": "複合公式生成器",
+      "Probability Table": "概率一覽表",
+      Fuel: "燃",
+      Ammunition: "彈",
+      Ammu: "彈",
+      Steel: "鋼",
+      Bauxite: "鋁",
+      "Dev. Mat.": "開發資材",
+      Secretary: "秘書艦",
+      "HQ Level": "提督等級",
+      "Torpedo Family": "水雷系",
+      "Gunboat Family": "炮艦系",
+      "Carrier Family": "航母系",
+      Require: "需要",
+      "Min ": "最低",
+      "No Result": "無結果",
+      Details: "詳細",
+      "Expected Resource Consumption": "期望出貨消耗",
+      "Get Target": "出貨",
+      Develop: "開發",
+      "Success Rate": "成功率",
+      "Failure Rate": "失敗率",
+      Name: "名稱",
+      Percentage: "概率",
+      Failed: "失敗",
+      Internal: "内部",
+      Rarity: "稀有度",
+      "(\\d{1,2}%) when (.*?) is available": "$2 加入列表時為 $1",
+      "Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)":
+        "僅資源投入達到\n$1\n時加入列表",
+      " ?Italian Flagship": "意艦旗艦",
+      "No records for Luigi Torelli and Aquila":
+        "Luigi Torelli、Aquila 無出貨記錄",
+      "Project Homepage": "項目頁面",
+      Feedback: "反饋",
+      "Data Source\\(Chinese\\)": "數據來源",
+    };
 
-itemtypenames = [ "小口徑主砲",
-"中口徑主砲",
-"大口徑主砲",
-"副砲",
-"機槍",
-"對空強化彈",
-"穿甲彈",
-"魚雷 & 微型潛艇",
-"艦載戰鬥機",
-"艦載魚雷機",
-"艦載轟炸機",
-"艦載偵查機",
-"水上機",
-"電探",
-"對潛裝備",
-"引擎",
-"增設裝甲板",
-"陸基攻擊機",
-"簡易運輸桶" ]
-	} else if (lang == "ja") {
-document.title = "艦これ　装備開発ツール"
-$(function(){document.body.lang = "ja"})
-dict = { "KanColle Development Tools": "艦これ　装備開発ツール",
-"Development Simulator & Recipe Generator": "開発シミュレータ&複合レシピジェネレータ",
-"Development Simulator": "開発シミュレータ",
-"Recipe Generator": "複合レシピジェネレータ",
-"Probability Table": "確率一覧表",
-"Fuel": "燃料",
-"Ammunition": "弾薬",
-"Ammu": "弾薬",
-"Steel": "鋼材",
-"Bauxite": "ボーキ",
-"Dev. Mat.": "開発資材",
-"Secretary": "秘書艦",
-"HQ Level": "司令部LV",
-"Torpedo Family": "水雷系",
-"Gunboat Family": "砲戦系",
-"Carrier Family": "空母系",
-"Min ": "最低",
-"No Result": "結果がない",
-"Details": "詳細",
-"Expected Resource Consumption": "予想した資源の使用量",
-"Get Target": "目安を入手",
-"Develop": "開発",
-"Success Rate": "成功率",
-"Failure Rate": "失敗率",
-"Name": "名称",
-"Percentage": "確率",
-"Failed": "失敗",
-"Internal": "内部の",
-"Rarity": "レア度",
-"(\\d{1,2}%) when (.*?) is available": "$2 が出る時の確率は $1",
-"Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)": "材料使用量が\n$1\n以上の場合のみ",
-"Require Italian Flagship": "イタリア艦を秘書艦に\n任命する必要があります",
-"Require(<br ?/?>| )([\\w\\W]*)": "$2$1を必要とする",
-"Italian Flagship": "イタリア艦を秘書艦に任命する",
-"No records for Luigi Torelli and Aquila": "Luigi Torelli と Aquila に関する記録はありません",
-"Project Homepage": "ホームページへ",
-"Feedback": "フィードバック",
-"Data Source\\(Chinese\\)": "データのソース（中国語）" }
+    itemtypenames = [
+      "小口徑主砲",
+      "中口徑主砲",
+      "大口徑主砲",
+      "副砲",
+      "機槍",
+      "對空強化彈",
+      "穿甲彈",
+      "魚雷 & 微型潛艇",
+      "艦載戰鬥機",
+      "艦載魚雷機",
+      "艦載轟炸機",
+      "艦載偵查機",
+      "水上機",
+      "電探",
+      "對潛裝備",
+      "引擎",
+      "增設裝甲板",
+      "陸基攻擊機",
+      "簡易運輸桶",
+	  "高射裝置",
+	  "陸軍戰鬥機",
+    ];
+  } else if (lang == "ja") {
+    document.title = "艦これ　装備開発ツール";
+    $(function () {
+      document.body.lang = "ja";
+    });
+    dict = {
+      "KanColle Development Tools": "艦これ　装備開発ツール",
+      "Development Simulator & Recipe Generator":
+        "開発シミュレータ&複合レシピジェネレータ",
+      "Development Simulator": "開発シミュレータ",
+      "Recipe Generator": "複合レシピジェネレータ",
+      "Probability Table": "確率一覧表",
+      Fuel: "燃料",
+      Ammunition: "弾薬",
+      Ammu: "弾薬",
+      Steel: "鋼材",
+      Bauxite: "ボーキ",
+      "Dev. Mat.": "開発資材",
+      Secretary: "秘書艦",
+      "HQ Level": "司令部LV",
+      "Torpedo Family": "水雷系",
+      "Gunboat Family": "砲戦系",
+      "Carrier Family": "空母系",
+      "Min ": "最低",
+      "No Result": "結果がない",
+      Details: "詳細",
+      "Expected Resource Consumption": "予想した資源の使用量",
+      "Get Target": "目安を入手",
+      Develop: "開発",
+      "Success Rate": "成功率",
+      "Failure Rate": "失敗率",
+      Name: "名称",
+      Percentage: "確率",
+      Failed: "失敗",
+      Internal: "内部の",
+      Rarity: "レア度",
+      "(\\d{1,2}%) when (.*?) is available": "$2 が出る時の確率は $1",
+      "Only available when<br ?/?>material is greater than<br ?/?>([0-9\\/]*)":
+        "材料使用量が\n$1\n以上の場合のみ",
+      "Require Italian Flagship":
+        "イタリア艦を秘書艦に\n任命する必要があります",
+      "Require(<br ?/?>| )([\\w\\W]*)": "$2$1を必要とする",
+      "Italian Flagship": "イタリア艦を秘書艦に任命する",
+      "No records for Luigi Torelli and Aquila":
+        "Luigi Torelli と Aquila に関する記録はありません",
+      "Project Homepage": "ホームページへ",
+      Feedback: "フィードバック",
+      "Data Source\\(Chinese\\)": "データのソース（中国語）",
+    };
 
-itemtypenames = [ "小口径主砲",
-"中口径主砲",
-"大口径主砲",
-"副砲",
-"対空機銃",
-"対空強化弾",
-"対艦強化弾",
-"魚雷 & 特殊潜航艇",
-"艦上戦闘機",
-"艦上攻撃機",
-"艦上爆撃機",
-"艦上偵察機",
-"水上機",
-"レーダー",
-"ソナー & 爆雷",
-"機関部強化",
-"増設バルジ",
-"陸上攻撃機",
-"簡易輸送部材" ]
-	} else $(function(){document.body.lang = "en"})
-	document.addEventListener('DOMContentLoaded', function() {
-		var $cs = $(".i18n")
-		for (var i = 0; i < $cs.length; ++i) $cs[i].innerHTML = getString($cs[i].innerHTML)
-		$cs = $(".notice")
-		for (var i = 0; i < $cs.length; ++i) $cs[i].title = getString($cs[i].title)
+    itemtypenames = [
+      "小口径主砲",
+      "中口径主砲",
+      "大口径主砲",
+      "副砲",
+      "対空機銃",
+      "対空強化弾",
+      "対艦強化弾",
+      "魚雷 & 特殊潜航艇",
+      "艦上戦闘機",
+      "艦上攻撃機",
+      "艦上爆撃機",
+      "艦上偵察機",
+      "水上機",
+      "レーダー",
+      "ソナー & 爆雷",
+      "機関部強化",
+      "増設バルジ",
+      "陸上攻撃機",
+      "簡易輸送部材",
+	  "高射装置",
+	  "陸軍戦闘機",
+    ];
+  } else
+    $(function () {
+      document.body.lang = "en";
+    });
+  document.addEventListener("DOMContentLoaded", function () {
+    var $cs = $(".i18n");
+    for (var i = 0; i < $cs.length; ++i)
+      $cs[i].innerHTML = getString($cs[i].innerHTML);
+    $cs = $(".notice");
+    for (var i = 0; i < $cs.length; ++i) $cs[i].title = getString($cs[i].title);
 
-		$("#lang").find("[value=" + lang + "]")[0].selected = true
-		$("#lang")[0].onchange = function() {
-			location.href = "?" + $("#lang")[0].selectedOptions[0].value + window.location.hash
-		}
-	})
-}())
-var dict
+    $("#lang").find("[value=" + lang + "]")[0].selected = true;
+    $("#lang")[0].onchange = function () {
+      location.href =
+        "?" + $("#lang")[0].selectedOptions[0].value + window.location.hash;
+    };
+  });
+})();
+var dict;
 function getString(src) {
-	if (dict == undefined) return src
-	for (var name in dict) {
-		src = src.replace(new RegExp(name, "g"), dict[name])
-	}
-	return src
+  if (dict == undefined) return src;
+  for (var name in dict) {
+    src = src.replace(new RegExp(name, "g"), dict[name]);
+  }
+  return src;
 }
